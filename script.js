27d32027cb9d6507fe958ca573e71b04e5d86a78
diego2go <67561210+diego2go@users.noreply.github.com>
2023-04-13 18:14:51 -0600

    Add variables firstNum, operator and secondNum

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ function multiply (a,b) {
 function divide (a,b) {
     return a / b;
 }
+
+let firstNum = 0; //for initial testing. to be set document.querySelector('.firstNum')
+let operator = '+' ; //for initial testing. to be set document.querySelector('.operator')
+let secondNum = 0; //for initial testing. to be set document.querySelector('.secondNum')
+
