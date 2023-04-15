83cab54524a7d421b838c2f13c415d9a600223b2
diego2go <67561210+diego2go@users.noreply.github.com>
2023-04-15 17:33:47 -0600

    Add vars to store nums and display selection

--- script.js
+++ script.js
@@ -26,3 +26,17 @@ function operate (operator, a, b) {
     // Same as 
     // if (operator === '+') return add(a, b); // for each op...
 }
+
+const displayNum = document.querySelectorAll('.number');
+const currentSelDisplay = document.querySelector('.current');
+
+let displayValue = 0;
+
+displayNum.forEach((num) => {
+    num.addEventListener('click', (e) => {
+        (displayValue === 0) ? displayValue = num.textContent : 
+        displayValue += num.textContent;
+        currentSelDisplay.textContent = displayValue;
+        console.log(currentSelDisplay.textContent)
+    })
+})
